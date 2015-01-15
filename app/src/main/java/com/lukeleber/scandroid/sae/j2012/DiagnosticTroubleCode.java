4fb55837c20dea2c5847de22fc509fb67af850e1
Luke <lukeleber@gmail.com>
2015-01-15 18:58:25 -0500

    Updated copyright

--- app/src/main/java/com/lukeleber/scandroid/sae/j2012/DiagnosticTroubleCode.java
+++ app/src/main/java/com/lukeleber/scandroid/sae/j2012/DiagnosticTroubleCode.java
@@ -1,9 +1,20 @@
-/*
- * This file is protected under the KILLGPL.
- * For more information visit <insert_valid_link_to_killgpl_here>
- * <p/>
- * Copyright (c) Luke A. Leber <LukeLeber@gmail.com> 2014
- */
+// This file is protected under the KILLGPL.
+// For more information, visit http://www.lukeleber.github.io/KILLGPL
+//
+// Copyright (c) Luke Leber <LukeLeber@gmail.com>
+//
+// "This software is provided freely under a single condition.  You can't force anyone
+// that uses this software to release any source code.  It is their decision alone.
+// Other than that, anything goes.
+//
+// PS. If you like this work, please hire me!  I'm uneducated (no CS degree)
+// and as such its borderline impossible to get into the industry.  My current
+// career is absolute hell and I am trapped in it.  I leave this message here
+// in the vain hope that someone, somewhere might read this and give me the
+// opportunity to join their association as a programmer."
+//
+// Cheers,
+// Luke
 
 package com.lukeleber.scandroid.sae.j2012;
 
