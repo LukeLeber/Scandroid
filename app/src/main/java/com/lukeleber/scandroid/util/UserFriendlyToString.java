ddbe390b0057ff980e2d05374750fdefe3f0b02f
Luke <LukeLeber@gmail.com>
2015-01-23 17:38:29 -0500

    Initial commit - The OBDII SAE documents mandate the terminology and abbreviations that are required to be displayed to the user.  These terminologies are generated by the default toString mechanism.  The UserFriendlyToString interface provides an alternative "user friendly" way to express things.

--- app/src/main/java/com/lukeleber/scandroid/util/UserFriendlyToString.java
+++ app/src/main/java/com/lukeleber/scandroid/util/UserFriendlyToString.java
@@ -1,11 +1,5 @@
 package com.lukeleber.scandroid.util;
 
-/**
- * This file is protected under the KILLGPL. For more information visit
- * https://www.github.com/lukeleber/KILLGPL
- * <p/>
- * Copyright Luke <LukeLeber@gmail.com> 1/12/2015.
- */
 public interface UserFriendlyToString
 {
     String toUserFriendlyString();
