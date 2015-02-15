c8778e4ca104c547121d7cc878927323d1b507d1
Luke <lukeleber@gmail.com>
2015-02-14 21:18:09 -0500

    Documentation Enhancement

--- src/main/java/com/lukeleber/scandroid/util/Internationalized.java
+++ src/main/java/com/lukeleber/scandroid/util/Internationalized.java
@@ -7,7 +7,22 @@ package com.lukeleber.scandroid.util;
 
 import android.content.Context;
 
+/**
+ * The {@link com.lukeleber.scandroid.util.Internationalized} interface provides an alternative
+ * to the {@link #toString()} method that allows for an internationalized string to be returned
+ * based upon the {@link android.content.Context} that the method is invoked with.
+ *
+ */
 public interface Internationalized
 {
+
+    /**
+     * Retrieves an internationalized {@link java.lang.String} representation of this object
+     *
+     * @param context the calling {@link android.content.Context}
+     *
+     * @return an internationalized {@link java.lang.String} representation of this object
+     *
+     */
     String toI18NString(Context context);
 }
