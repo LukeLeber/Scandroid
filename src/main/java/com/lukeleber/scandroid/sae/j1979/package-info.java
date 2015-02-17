08dc154ced18327117ba17010c053b0bd83cdc77
Luke <lukeleber@gmail.com>
2015-02-17 18:21:19 -0500

    updated documentation ( Internationalized#toI18NString(Context) did not exist at the time of initial docs)

--- src/main/java/com/lukeleber/scandroid/sae/j1979/package-info.java
+++ src/main/java/com/lukeleber/scandroid/sae/j1979/package-info.java
@@ -9,11 +9,12 @@
  * this package:
  * <ol>
  *     <li>Standard terminology (per SAE-J1979)</li>
- *     <li>"User Friendly" terminology (no abbreviations and/or acronyms)</li>
+ *     <li>"User Friendly" terminology (internationalized, no abbreviations and/or acronyms)</li>
  * </ol>
  *
  * <p>Standard terminology is provided by the {@link java.lang.Object#toString()} method, whereas
  * the user-friendly terminology is provided by the
- * {@link com.lukeleber.scandroid.util.Internationalized#toUserFriendlyString(Context)} method.</p>
+ * {@link com.lukeleber.scandroid.util.Internationalized#toI18NString(android.content.Context)}
+ * method.</p>
  */
 package com.lukeleber.scandroid.sae.j1979;
